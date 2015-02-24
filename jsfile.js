089ea8a4be2b14e4510700d0909198e1b4f6eb9a
alpranzetti <alpranzetti1@gmail.com>
2015-02-24 15:44:42 -0300

    Update jsfile.js

---
+++ jsfile.js
@@ -0,0 +1,35 @@
+function MOVIE (moviename)
+{ 
+    this.name = moviename ;
+    this.play = play; 
+    this.stop = stop; 
+    this.get = get;
+    this.set = set; 
+}
+
+MOVIE1 = new MOVIE ( "The Unexpected Virtue of Ignorance");
+MOVIE2= new MOVIE ( "The Theory of Everything");
+MOVIE3= new MOVIE ( "The Imitation Game");
+
+
+
+function play()
+{
+    document.write(movie); 
+}
+
+function stop()
+{
+
+}
+
+function set(string , value)
+{
+    
+}
+
+function get(string)
+{
+    
+}
+
